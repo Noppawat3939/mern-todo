fc47af9bfba5713050c6f10fa72ab57322962a8f
GG_NC <99155577+Noppawat3939@users.noreply.github.com>
2023-02-02 22:15:26 +0700

    add create todo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,15 @@ const URL = "http://localhost:3001";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
   useEffect(() => {
     getTodos();
   }, []);
+
+  const onChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
   const getTodos = () => {
     fetch(`${URL}/todos`)
@@ -35,9 +40,42 @@ const App = () => {
     setTodos(completeTask);
   };
 
+  const deleteTodo = async (id) => {
+    const data = await fetch(`${URL}/todo/delete/${id}`, {
+      method: "DELETE",
+    }).then((res) => res.json());
+
+    const deleteTask = todos.filter((todo) => todo._id !== data._id);
+
+    setTodos(deleteTask);
+  };
+
+  const createTodo = async () => {
+    const newTodo = await fetch(`${URL}/todo/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ task: inputValue }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
+
   return (
     <div className="container">
       <h1>MERN Todo</h1>
+      <input
+        className="add_input"
+        onChange={(e) => onChange(e)}
+        type="text"
+        value={inputValue}
+        placeholder="add your task is here..."
+      />
+      <button className="add_btn" onClick={createTodo}>
+        add
+      </button>
       <div>
         {!todos.length ? (
           <h3>no todo</h3>
@@ -51,10 +89,7 @@ const App = () => {
                 >
                   {task}
                 </p>
-                <button
-                  className="delete_btn"
-                  onClick={() => console.log("delete")}
-                >
+                <button className="delete_btn" onClick={() => deleteTodo(_id)}>
                   delete
                 </button>
               </li>
